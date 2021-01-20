3f8606027104bf590540b5773dcc02c68ca40fee
Olga Thompson <olgatj2007@gmail.com>
2021-01-20 16:43:04 -0500

    finished phase 1

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 
+const store = configureStore();
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
 function Root() {
   return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
